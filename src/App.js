ed82b39ae124c1c2458393671202b5ff865d10f9
damtea <damtearlte@gmail.com>
2019-04-26 18:16:54 +0530

    First

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>React Page </h1>
+        <br />
+        <a
+          className="button-line"
+          href="https://github.com/deityhub"
+          target="_blank"
+        >
+          Know more now
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default App;
